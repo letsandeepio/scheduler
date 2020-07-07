bff64657c9b65950ce95b5e2beb4c73a02b1b1af
letsandeepio <sandeepchopra7@gmail.com>
2020-07-07 18:10:04 -0400

    creates & updates Appointment component

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Header from 'components/Appointment/Header';
+import Show from 'components/Appointment/Show';
+
+import 'components/Appointment/styles.scss';
+
+export default function Appointment() {
+  return <article className="appointment"></article>;
+}
