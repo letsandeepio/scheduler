5bed0992e4bcd970155ab4d74305310756cf4765
letsandeepio <sandeepchopra7@gmail.com>
2020-07-08 17:40:04 -0400

    removes hardcoded appointment object

--- src/components/Application.js
+++ src/components/Application.js
@@ -7,61 +7,6 @@ import 'components/Application.scss';
 import DayList from 'components/DayList';
 import Appointment from 'components/Appointment';
 
-const appointments = [
-  {
-    id: 1,
-    time: '12pm'
-  },
-  {
-    id: 2,
-    time: '1pm',
-    interview: {
-      student: 'Lydia Miller-Jones',
-      interviewer: {
-        id: 1,
-        name: 'Sylvia Palmer',
-        avatar: 'https://i.imgur.com/LpaY82x.png'
-      }
-    }
-  },
-  {
-    id: 3,
-    time: '2pm',
-    interview: {
-      student: 'King John IV',
-      interviewer: {
-        id: 1,
-        name: 'Rockstar Baby',
-        avatar: 'https://i.imgur.com/T2WwVfS.png'
-      }
-    }
-  },
-  {
-    id: 4,
-    time: '3pm',
-    interview: {
-      student: 'Frodo',
-      interviewer: {
-        id: 1,
-        name: 'Balbir Singh',
-        avatar: 'https://i.imgur.com/FK8V841.jpg'
-      }
-    }
-  },
-  {
-    id: 5,
-    time: '4pm',
-    interview: {
-      student: 'Dwayne Johnson',
-      interviewer: {
-        id: 1,
-        name: 'Peter Parker',
-        avatar: 'https://i.imgur.com/twYrpay.jpg'
-      }
-    }
-  }
-];
-
 export default function Application(props) {
   const [state, setState] = useState({
     day: 'Monday',
