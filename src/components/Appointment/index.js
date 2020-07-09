361c4de434f25901d192e295a65e9108a327a2b5
letsandeepio <sandeepchopra7@gmail.com>
2020-07-09 17:13:35 -0400

    adds useVisualMode hook to appointment module, and enable add & transition states

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,14 +2,28 @@ import React from 'react';
 import Header from 'components/Appointment/Header';
 import Show from 'components/Appointment/Show';
 import Empty from 'components/Appointment/Empty';
+import Form from 'components/Appointment/Form';
+
+import useVisualMode from 'hooks/useVisualMode';
 
 import 'components/Appointment/styles.scss';
 
-export default function Appointment({ time, interview }) {
+const EMPTY = 'EMPTY';
+const SHOW = 'SHOW';
+const CREATE = 'CREATE ';
+
+export default function Appointment({ time, interview, onAdd }) {
+  const initMode = interview ? SHOW : EMPTY;
+  const { mode, transition, back } = useVisualMode(initMode);
+
   return (
     <article className="appointment">
       <Header time={time} />
-      {interview ? <Show {...interview} /> : <Empty />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show student={interview.student} interviewer={interview.interviewer} />
+      )}
+      {mode === CREATE && <Form interviewers={[]} onCancel={() => back()} />}
     </article>
   );
 }
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,10 +4,17 @@ export default function useVisualMode(initMode) {
   const [mode, setMode] = useState(initMode);
   const [history, setHistory] = useState([initMode]);
 
-  function transition(toMode) {
+  function transition(toMode, replace = false) {
     setMode(toMode);
-    setHistory([...history, toMode]);
+    if (replace) {
+      let replaceArray = [...history];
+      replaceArray[replaceArray.length - 1] = toMode;
+      setHistory(replaceArray);
+    } else {
+      setHistory([...history, toMode]);
+    }
   }
+
   function back() {
     if (history.length > 1) {
       const removeLast = [...history];
