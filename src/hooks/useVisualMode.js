ee9359267b134ddd88ab93d21a034684a99ca32f
letsandeepio <sandeepchopra7@gmail.com>
2020-07-09 13:51:58 -0400

    implements tests & function for back transition in useVisualMode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,11 +2,21 @@ import { useState } from 'react';
 
 export default function useVisualMode(initMode) {
   const [mode, setMode] = useState(initMode);
+  const [history, setHistory] = useState([initMode]);
+
   function transition(toMode) {
     setMode(toMode);
+    setHistory([...history, toMode]);
+  }
+  function back() {
+    const removeLast = [...history];
+    removeLast.pop();
+    setHistory(removeLast);
+    setMode(removeLast[removeLast.length - 1]);
   }
   return {
     mode,
-    transition
+    transition,
+    back
   };
 }
