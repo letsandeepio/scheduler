90a7f5740fda77ce8b4f5ba3ee9cc0edcb72f7b4
letsandeepio <sandeepchopra7@gmail.com>
2020-07-16 17:42:40 -0400

    updates the tests to cover the last bug fix

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -41,7 +41,11 @@ describe('Form', () => {
   it('can successfully save after trying to submit an empty student name', () => {
     const onSave = jest.fn();
     const { getByText, getByPlaceholderText, queryByText } = render(
-      <Form interviewers={interviewers} onSave={onSave} />
+      <Form
+        interviewers={interviewers}
+        onSave={onSave}
+        interviewer={{ id: 2 }}
+      />
     );
 
     fireEvent.click(getByText('Save'));
@@ -58,7 +62,7 @@ describe('Form', () => {
     expect(queryByText(/student name cannot be blank/i)).toBeNull();
 
     expect(onSave).toHaveBeenCalledTimes(1);
-    expect(onSave).toHaveBeenCalledWith('Lydia Miller-Jones', null);
+    expect(onSave).toHaveBeenCalledWith('Lydia Miller-Jones', { id: 2 });
   });
 
   it('calls onCancel and resets the input field', () => {
